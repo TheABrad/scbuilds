bfaa3f03f934bb59269d0e9d16d8884045b115ab
TheABrad <alex.guerrilla@gmail.com>
2014-03-31 21:29:11 +0000

    builds full title has race matchup

--- app/models/build.rb
+++ app/models/build.rb
@@ -8,4 +8,12 @@ class Build < ActiveRecord::Base
   validates :body, presence: true
 
   scope :recent, order("builds.created_at DESC")
+
+  def matchup
+    self.race[0] + "v" + self.vs_race[0]
+  end
+
+  def full_title
+    matchup + ": " + self.title
+  end
 end
