136ba556b46f7d53edcf51ab183a59be72137d9c
TheABrad <alex.guerrilla@gmail.com>
2014-11-26 20:43:41 +0000

    added friendly_id for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-      @user = User.find(params[:id])
+      @user = User.friendly.find(params[:id])
       @builds = Build.where(user_id: current_user.id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  extend FriendlyId
+  friendly_id :username, use: :slugged
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :builds
+
+  def should_generate_new_friendly_id?
+    new_record? || slug.nil? || slug.blank?
+  end
 end
