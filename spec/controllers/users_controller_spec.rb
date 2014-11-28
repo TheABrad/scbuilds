32a2893c15ec013871c46f75b0ed582589033a8f
TheABrad <alex.guerrilla@gmail.com>
2014-11-28 09:17:37 -0500

    user profiles

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,6 @@ require 'spec_helper'
 
 describe UsersController do
 
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      expect(response).to be_success
-    end
-  end
+
 
 end
