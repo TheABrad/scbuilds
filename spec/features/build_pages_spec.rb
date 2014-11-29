90613009205c0aa3cb0270ca0ea7d5ed35cf45c4
TheABrad <alex.guerrilla@gmail.com>
2014-11-29 05:30:00 -0500

    Select in build forms and tests pass!

--- spec/features/build_pages_spec.rb
+++ spec/features/build_pages_spec.rb
@@ -1,26 +1,24 @@
 require 'spec_helper'
 
-describe "Build Pages" do
+describe 'Build Pages' do
   let(:user) { FactoryGirl.create(:user) }
 
   before { sign_in user }
- 
-  describe "Create a Build" do
-    before(:all) do 
+
+  describe 'Create a Build' do
+    before(:all) do
       visit new_build_path
     end
 
-    let(:build) { FactoryGirl.create(:build) }
+    describe 'Fill in Build' do
+      it 'fills in the build with correct information' do
+        fill_in 'Title', with: 'TitleBuild'
+        choose 'build_race_protoss'
+        choose 'build_vs_race_terran'
+        fill_in 'Body', with: 'Lorem Ipsum'
+        fill_in 'Replays', with: 'http://drop.sc/11111'
 
-    describe "Fill in Build" do
-      it "fill's in the build with correct information" do 
-        select('Protoss', :from => "Race") 
-        select('Terran', :from => "Vs Race") 
-        fill_in "Title", with: build.title
-        fill_in "Body", with:  build.body  
-	    fill_in "Replays", with: build.replays
-       
-       	click_button "Create Build"
+        expect { click_button 'Create Build' }.to change(Build, :count).by(1)
       end
     end
   end
--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 
-describe "UserPages" do
-  describe "account creation" do
-    it "allows user to create account" do
+describe 'UserPages' do
+  describe 'account creation' do
+    it 'allows user to create account' do
       visit new_user_registration_path
-      
-      fill_in "Username", with: "ExampleUser"
-      fill_in "Email", with: "user@example.com"
-      fill_in "Password", with: "foobarbaz"
-      fill_in "Password confirmation", with: "foobarbaz" 
-      
-      expect { click_button 'Sign up' }.to change(User, :count).by(1) 
+
+      fill_in 'Username', with: 'ExampleUser'
+      fill_in 'Email', with: 'user@example.com'
+      fill_in 'Password', with: 'foobarbaz'
+      fill_in 'Password confirmation', with: 'foobarbaz'
+
+      expect { click_button 'Sign up' }.to change(User, :count).by(1)
     end
   end
 end
